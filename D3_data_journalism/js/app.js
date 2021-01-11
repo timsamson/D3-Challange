737fc46c65b465bba100586955274f6257379a27
Tim Samson <9625746+timsamson@users.noreply.github.com>
2021-01-11 17:00:40 -0500

    lables

--- D3_data_journalism/js/app.js
+++ D3_data_journalism/js/app.js
@@ -7,7 +7,7 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper
-var svg = d3.select(".chart")
+var svg = d3.select(".scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
@@ -16,65 +16,81 @@ var svg = d3.select(".chart")
 var chart = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// Retrieve data from the CSV file and execute everything below
-d3.csv("D3_data_journalism/data/data.csv").then(function(demoData) {
+  // Initial Params
+var chosenXAxis = "healthcare";
 
-  // Step 1: Parse Data/Cast as numbers
-  // ==============================
+//Data
+var demoData = await d3.csv("D3_data_journalism/data/data.csv");
+
+// Parse Data/Cast as numbers
   demoData.forEach(function(data) {
-    data.obesity = +data.obesity;
-    data.income = +data.income;
     data.healthcare = +data.healthcare;
+    data.obesity    = +data.obesity;
+    data.income     = +data.income;
+  });
 
-    });
+//  scale functions
+var xLinearScale = xScale(demoData, chosenXAxis);
+var yLinearScale = yScale(demoData, chosenYAxis);
 
-    console.log(demoData);
-
-// Create scales
-var xLinearScale = d3.scaleLinear()
-  .domain([20, d3.max(demoData, d => d.healthcare)])
-  .range([0, width]);
-
-var yLinearScale = d3.scaleLinear()
-  .domain([0, d3.max(demoData, d => d.obesity)])
-  .range([height, 0]);
-  
-// Create Axis
+// Init axis functions
 var bottomAxis = d3.axisBottom(xLinearScale);
 var leftAxis = d3.axisLeft(yLinearScale);
-  
-// amend axis(s) to chart
-chart.append("g")
-.attr("transform", `translate(0, ${height})`)
-.call(bottomAxis);
 
-chart.append("g")
-.call(leftAxis);
+// Append x and y axes to the chart
+var xAxis = chartGroup.append("g")
+  .attr("transform", `translate(0, ${height})`)
+  .call(bottomAxis);
 
-//Create Circles Draft
-var circlesGroup = chart.selectAll("circle")
-.data(demoData)
-.enter()
-.append("circle")
-.attr("cx", d => xLinearScale(d.obesity))
-.attr("cy", d => yLinearScale(d.healthcare))
-.attr("r", "15")
-.attr("fill", "pink")
-.attr("opacity", ".5");
+var yAxis = chartGroup.append("g")
+  .call(leftAxis);
 
-//Init tool tip
-var toolTip = d3.tip()
-.attr("class", "tooltip")
-.offset([80, -60])
-.html(function(d) {
-  return (`${d.income }`);
-});
+// Create circles
+var circlesGroup = chartGroup.selectAll("g circle")
+  .data(stateData)
+  .enter()
+  .append("g");
 
-// create chart
-chart.call(toolTip);
+var circlesXY = circlesGroup.append("circle")
+  .attr("cx", d => xLinearScale(d[chosenXAxis]))
+  .attr("cy", d => yLinearScale(d[chosenYAxis]))
+  .attr("r", 15)
+  .classed("demoCircle", true);
 
+var circlesText = circlesGroup.append("text")
+  .text(d => d.abbr)
+  .attr("dx", d => xLinearScale(d[chosenXAxis]))
+  .attr("dy", d => yLinearScale(d[chosenYAxis]) + 5)
+  .classed("demoText", true);
 
+// X labels
+var xlabelsGroup = chartGroup.append("g")
+  .attr("transform", `translate(${width / 2}, ${height})`);
 
+var incomeLabel = xlabelsGroup.append("text")
+  .attr("x", 0)
+  .attr("y", 80)
+  .attr("value", "income") 
+  .text("Household Income (Median)")
+  .classed("inactive", true);
 
+// Y labels
+var ylabelsGroup = chartGroup.append("g");
 
-  });
+var healthcareLabel = ylabelsGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -(height / 2))
+    .attr("y", -40)
+    .attr("value", "healthcare") 
+    .text("Lacks Healthcare (%)")
+    .classed("active", true);
+
+var obeseLabel = ylabelsGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -(height / 2))
+    .attr("y", -80)
+    .attr("value", "obesity") 
+    .text("Obese (%)")
+    .classed("inactive", true);
+
+//Tool Tips
