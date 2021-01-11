9dce6827fd8212074dc8c9394a92946ee851cfdb
Tim Samson <9625746+timsamson@users.noreply.github.com>
2021-01-11 13:48:04 -0500

    SVG setup

--- D3_data_journalism/js/app.js
+++ D3_data_journalism/js/app.js
@@ -1,1 +1,22 @@
-// @TODO: YOUR CODE HERE!
+var svgWidth = 900;
+var svgHeight = 500;
+
+var margin = { top: 20, right: 40, bottom: 80, left: 100 };
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper
+var svg = d3
+  .select(".chart")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// Append an SVG group
+var chart = svg.append("g");
+
+// Append a div
+d3.select(".chart").append("div").attr("class", "tooltip").style("opacity", 0);
