9ac20683da24349b279fff22873ff05ec6d7515c
Tim Samson <9625746+timsamson@users.noreply.github.com>
2021-01-16 11:53:18 -0500

    corrected spelling in analysis copy

--- D3_data_journalism/js/functions.js
+++ D3_data_journalism/js/functions.js
@@ -132,43 +132,43 @@ function writeAnalysis (chosenXAxis, chosenYAxis){
   if (chosenXAxis === "poverty") {
     if (chosenYAxis === "healthcare") {
       articleHeader = "Poverty and the Uninsured"
-      articleText = "Individuals in low-income households are more likely to be uninsured than those in higher income households, not all uninsured are also low income. Texas, exhibits the the highest number of uninsured, but does not also exhibit the highest number of families in poverty. Other factors appear to be contributing to whether an individual is insured."
+      articleText = "Individuals in low-income households are more likely to be uninsured than those in higher income households, not all uninsured are also low income. Texas, exhibits the the highest number of uninsured, but does not also exhibit the highest number of families in poverty. Other factors appear to be contributing to rate of an individual being insured."
     }
     else if(chosenYAxis === "smokes") {
       articleHeader = "Poverty and Smoking"
-      articleText = "The dataset protrays that invividuals living in the U.S., below the poverty level are more likely to smoke cigarettes than the general population. Poverty does not appear to be isolated with it's affect on smoking rates, states like California and New York appear around the median for poverty rates, yet still exhibit the lowest rates of smoking relative to populations."
+      articleText = "The dataset protrays that invividuals living in the U.S., below the poverty level are more likely to smoke cigarettes than the remainder of the population. Poverty does not appear to be isolated with it's affect on smoking rates. California and New York appear around the median for poverty rates, yet exhibit the lowest rates of smoking relative to overall populations."
     }
     else {
       articleHeader = "Poverty and Obesity"
-      articleText = "Overall, the dataset shows high obesity rates; it suggests that individuals who live in impoverished regions have poor access to fresh food. Food Deserts, or those regions with limited access to food often oorespond to those areas which are also below the porverty rate. This dataset shows there are states, New Hampshire for example, that have very low poverty rates but still have relatively higher rates of obesity, suggeting other factors may also be involved."
+      articleText = "The dataset reveals high obesity rates; and suggests that individuals who live in impoverished regions have higher rates of obesity. One contributing factor (not explictly addressed in the dataset) could be Food Deserts, or those regions with limited access to food often coorespond to those areas which are also below the porverty rate. New Hampshire has very low poverty rates but still have relatively higher rates of obesity, suggeting other factors may also be involved."
     }
   }
   else if (chosenXAxis === "age"){
     if (chosenYAxis=== "healthcare") {
       articleHeader = "Age and the Uninsured"
-      articleText = "It is suggested that younger indiviudals lack healthinsurance due toa  number of factors, so it would make senese that those regions that have a younger demographic would also have lower rates of indidvuals with insurace coverage. Massachusetts appears to have the lowest number of uninsured individuals, most likely due to thier universal state healthcare. Texas conversly, has the highest rate of indivudals with no insurance, but a much lower median age."
+      articleText = "It is widely suggested that younger indiviudals lack health insurance, so it would seem likely that the data would show that regions that have a younger demographic would also have lower rates of indidvuals with insurace coverage. Massachusetts appears as a distinct outlier to this hypothosis since it displays the lowest number of uninsured individuals, this likely is due to thier the states universal healthcare Program. Texas conversly, has the highest rate of indivudals with no insurance, but a much lower median age."
     }
     else if(chosenYAxis === "smokes") {
       articleHeader = "Age and Smoking"
-      articleText = "States with older populations are likely to have the higher rates of smoking. an observation might be that states where the median age is higher, may contain indivuals who ahve smoked for years of decades, whereas states such as Utah, with very low median age, also have lower smoking rates."
+      articleText = "States with older populations are likely to have the higher rates of smoking. An observation might concluded that states where the median age is higher, may contain indivuals who ahve smoked for longer periods of time and are less likley to quit. States such as Utah, appear as outliers, with very low median age and lower smoking rates."
     }
     else {
       articleHeader = "Age and Obesity"
-      articleText = "There does appear to be slight correlation between a states median age and level of obesity. It seems that higher age groups are more succeptible to being obese, but that is likely not always the case. Older age groups are typically less active than younger groups, and would therefore do much to explain why an older person is more likely to be obese. Clearly there are other factors, like poverty and income that may affect obesity levels."
+      articleText = "There does appear to be slight correlation between a states median age and level of obesity. It seems that higher average age groups are more succeptible to being obese, but that is likely a generalization of the aggregatte data. Older age groups are typically less active than younger groups, and could therefore explain why an older person is more likely to be obese. Other factors, like poverty and income likely contirnbute to obesity levels."
     }
   }
   else {
     if (chosenYAxis === "healthcare") {
       articleHeader = "Income and the Uninsured"
-      articleText = "Insurance coverage is costly in most areas, and this fact may be a factor in the rate of those uninsirued in lower median income areas. It would likely be expected that areas whith greater median income would alos have lower rates of those without insurance. Texas as the higehst level of uninsured individuals in the country."
+      articleText = "Insurance coverage is costly in most areas, and this fact may be a factor in the rate of those uninsirued cooresponding to lower median income areas. It would likely be expected that areas whith greater median income would also exhibit lower rates of individuals without insurance. Texas as the higehst level of uninsured individuals in the country."
     }
     else if(chosenYAxis === "smokes") {
       articleHeader = "Income and Smoking"
-      articleText = "States with high rates of poverty tend to have a greater population of individuals that smoke. States with lower median household income tend to also exhibit higher rates of smoking. Conversly, states with relatively higher median incomes have lower rates of smoking on average."
+      articleText = "States with high rates of poverty have a greater population of individuals that smoke on average. Likewise, states with lower median household income also exhibit higher rates of smoking. Generally, states with relatively higher median incomes have lower rates of smoking on average, pointing to income exhibiting a strong coorelation to smoking rates. "
     }
     else {
       articleHeader = "Income and Obesity";
-      articleText = "Overall obesity decreases with increased levels of income. Similarly to how poverty affects the access to healthy food, income exhibits similar properties.";
+      articleText = "Generally obesity decreases with increased levels of median income. Similarly to how poverty affects the access to healthy food, income exhibits similar properties.";
     }
   }
   d3.select("#article_text")
